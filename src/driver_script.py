01e545d11ece8ce12c419b469f43032794763553
Anu2601 <ms.anu.2601@gmail.com>
2021-11-25 21:36:19 +0530

    Added image_preprocess function,separated X and Y in training data

--- src/driver_script.py
+++ src/driver_script.py
@@ -2,7 +2,7 @@ from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.sql import SQLContext
 import data_process_funcs as dpf
-
+import numpy as np
 sc = SparkContext()
 ssc = StreamingContext(sc, 5)
 sqlc = SQLContext(sc)
@@ -12,26 +12,15 @@ def driver_function(rdd):
 	json_string_list = rdd.take(1)
 	if len(json_string_list) < 1:
 		return
-	#Converting back of JSON strings to numpy array
 	numpy_batch = dpf.batch_convert(json_string_list[0])
-	X_train = numpy_batch[:,:-1]
-	Y_train = numpy_batch[:,-1]
-	
-	#preprocessing the training features
-	X_train_norm = dpf.image_preprocess(X_train)
-	
-	#DEBUG---To be deleted later!!!
 	print("Entered the driver function")
 	print("--------------------------------")
-	print("Xtrain:")
-	print(X_train)
-	print(X_train_norm)
-	print("Ytrain:")
-	print(Y_train)
-	print("================================")
-	
+	print(numpy_batch)
+	print(np.shape(numpy_batch))
+	print("--------------------------------")
+
 
 socket_stream.foreachRDD(driver_function)
 
 ssc.start()
-ssc.awaitTermination()
+ssc.awaitTermination() 
