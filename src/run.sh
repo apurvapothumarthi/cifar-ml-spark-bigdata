bbac87026b1af4c064dc0fb22a84e717c831b00a
alinaj2110 <anjalipraveen2110@gmail.com>
2021-12-04 21:29:39 +0530

    updation of run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
 rm -vf ../models/*.sav
-python3 stream.py -f cifar -b 1000 &
+python3 stream.py -f ../inputs/ -b 1000 &
 $SPARK_HOME/bin/spark-submit driver_script.py 2>../log.txt
+
+python3 stream.py -f ../inputs/ -b 1000 &
+$SPARK_HOME/bin/spark-submit clustering.py 2>../log.txt
