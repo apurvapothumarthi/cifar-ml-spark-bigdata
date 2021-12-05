2fdfd85230bb5c4e9ce652bb02745527f1e6d4c6
apurvapothumarthi <apurva.pothumarthi@gmail.com>
2021-12-05 19:04:53 +0530

    Created cluster predictions function

--- src/predictions.py
+++ src/predictions.py
@@ -1,19 +1,37 @@
 import numpy as np
+import pickle
 from pyspark import SparkContext 
 from pyspark.streaming import StreamingContext
-from custom_funcs import SequentialKMeans, model_export, batch_convert
+import custom_funcs as cf
 from os import listdir
 
-totallist = listdir("../models")
-clusterfiles = totallist[0]
+dir_path = "../models/"
+totallist = listdir(dir_path)
+clusterfile = totallist[0]
 modelfiles = totallist[1:-1]
+cluster_model = pickle.load(open(dir_path+clusterfile, 'rb'))
 
 sc = SparkContext()
 ssc = StreamingContext(sc, 5)
 socket_stream = ssc.socketTextStream("localhost", 6100)
 
+def cluster_predictions(rdd):
+	json_string_list = rdd.take(1)
+	if len(json_string_list) < 1:
+		return
+	numpy_batch = cf.batch_convert(json_string_list[0])
+	X_test = numpy_batch[:,:-1]
+	Y_test = numpy_batch[:,-1]
+	pred = cluster_model.predict(X_test)
+	print("--------------------------------")
+	print("model:",cluster_model)
+	print("pred:")
+	print(pred)
+	print("actual:")
+	print(Y_test)
+	print("================================")
 
-
+socket_stream.foreachRDD(cluster_predictions)
 ssc.start()
 ssc.awaitTermination(650)
 ssc.stop()
