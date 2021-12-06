deab95d0745f6044535182854fc452030c6e552d
alinaj2110 <anjalipraveen2110@gmail.com>
2021-12-06 10:12:15 +0530

    accepted merge changes in prediction.py

--- src/predictions.py
+++ src/predictions.py
@@ -12,29 +12,20 @@ modelfiles = totallist[1:-1]
 cluster_model = pickle.load(open(dir_path+clusterfile, 'rb'))
 print(clusterfile[:-3]+"csv")
 f = open(clusterfile[:-3]+"csv","wb+")
-<<<<<<< HEAD
-
 models = list(map(lambda x: pickle.load(open(dir_path+x, 'rb')),
 		modelfiles))
-=======
->>>>>>> a431ee2b8d10ebe416ea4487e41159fa7de773eb
 
 sc = SparkContext()
 ssc = StreamingContext(sc, 5)
 socket_stream = ssc.socketTextStream("localhost", 6100)
 
-<<<<<<< HEAD
 def model_predictions(rdd):
-=======
-def cluster_predictions(rdd):
->>>>>>> a431ee2b8d10ebe416ea4487e41159fa7de773eb
 	json_string_list = rdd.take(1)
 	if len(json_string_list) < 1:
 		return
 	numpy_batch = cf.batch_convert(json_string_list[0])
 	X_test = numpy_batch[:,:-1]
 	Y_test = numpy_batch[:,-1]
-<<<<<<< HEAD
 	X_test_norm = cf.image_preprocess(X_test)
 	print("--------------------------------")
 	for model in models:
@@ -64,17 +55,6 @@ def cluster_predictions(rdd):
 	print("================================")
 
 socket_stream.foreachRDD(model_predictions)
-=======
-	pred = cluster_model.predict(X_test)
-	pred_Y = np.vstack((pred, Y_test)).T
-	np.savetxt(f,pred_Y,fmt="%d", delimiter=",")
-	print("--------------------------------")
-	print("model:",cluster_model)
-	print("pred:")
-	print(pred_Y)
-	print("================================")
-
->>>>>>> a431ee2b8d10ebe416ea4487e41159fa7de773eb
 socket_stream.foreachRDD(cluster_predictions)
 ssc.start()
 ssc.awaitTermination(650)
