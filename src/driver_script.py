a431ee2b8d10ebe416ea4487e41159fa7de773eb
alinaj2110 <anjalipraveen2110@gmail.com>
2021-12-06 09:00:52 +0530

    Added clustering algo to driverscript and deleted clustering script

--- src/driver_script.py
+++ src/driver_script.py
@@ -1,19 +1,22 @@
 from numpy import mod
 from pyspark import SparkContext 
 from pyspark.streaming import StreamingContext
-from pyspark.sql import SQLContext
+from pyspark.mllib.clustering import StreamingKMeans
 import custom_funcs as cf
 import sklearn.linear_model as sk_linear
-
+b = 3072*100
 sc = SparkContext()
 ssc = StreamingContext(sc, 5)
-sqlc = SQLContext(sc)
 socket_stream = ssc.socketTextStream("localhost", 6100)
 
 #global model
 Perceptron = sk_linear.Perceptron()
 SGDClassifier = sk_linear.SGDClassifier()
 PassiveAggressiveClassifier = sk_linear.PassiveAggressiveClassifier()
+
+#cluster model
+#streamingkmeans = cf.SequentialKMeans(n_cluster=10)
+Kcluster = cf.SequentialKMeans(10)
 
 def driver_function(rdd):
 	json_string_list = rdd.take(1)
@@ -22,18 +25,16 @@ def driver_function(rdd):
 	#Converting back of JSON strings to numpy array
 	numpy_batch = cf.batch_convert(json_string_list[0])
 	X_train = numpy_batch[:,:-1]
-	X_test = numpy_batch[-10:,:-1]
 	Y_train = numpy_batch[:,-1]
-	Y_test = numpy_batch[-10:,-1]
-	
+
 	#preprocessing the training features
 	X_train_norm = cf.image_preprocess(X_train)
-	X_test_norm = cf.image_preprocess(X_test)
 	
 	#partial_fit the models
 	Perceptron.partial_fit(X_train_norm, Y_train, classes=range(0,10))
 	SGDClassifier.partial_fit(X_train_norm, Y_train, classes=range(0,10))
 	PassiveAggressiveClassifier.partial_fit(X_train_norm, Y_train, classes=range(0,10))
+	Kcluster.fit(X_train)
 	'''
 	#DEBUG---To be deleted later!!!
 	print("Entered the driver function")
@@ -51,3 +52,4 @@ ssc.start()
 ssc.awaitTermination(650)
 ssc.stop()
 cf.model_export("ml",Perceptron,SGDClassifier,PassiveAggressiveClassifier)
+cf.model_export("cl",Kcluster)
