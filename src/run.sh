5d6a60c96fa5e29c03a7e71706f69e0c00fa8ad5
alinaj2110 <anjalipraveen2110@gmail.com>
2021-12-06 13:40:28 +0530

    included commnd to run predictions.py in run.sh

--- src/run.sh
+++ src/run.sh
@@ -4,5 +4,5 @@ rm -vf ../models/*.sav
 python3 stream.py -f ../inputs/ -b 1000 &
 $SPARK_HOME/bin/spark-submit driver_script.py 2>../log.txt
 
-python3 stream.py -f ../inputs/ -b 1000 &
-$SPARK_HOME/bin/spark-submit clustering.py 2>../log.txt
+python3 stream.py -f ../inputs/ -b 1000 -t True &
+$SPARK_HOME/bin/spark-submit predictions.py 2>../log.txt
